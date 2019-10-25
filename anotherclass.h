543d80490cb060f82b797e35b81d6fcb64f6de59
omriattal <omriattal@gmail.com>
2019-10-25 19:46:47 +0300

    added another class

---
+++ anotherclass.h
@@ -0,0 +1,15 @@
+//
+// Created by omrif on 10/25/2019.
+//
+
+#ifndef TESTING_ANOTHERCLASS_H
+#define TESTING_ANOTHERCLASS_H
+
+#include <iostream>
+
+class anotherclass {
+//hello
+};
+
+
+#endif //TESTING_ANOTHERCLASS_H
