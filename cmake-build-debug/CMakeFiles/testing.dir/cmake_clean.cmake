857915e3b9e2cd62416574c3f5d5bfa54671b0d9
omriattal <omriattal@gmail.com>
2019-10-25 11:53:04 +0300

    this is a message

---
+++ cmake-build-debug/CMakeFiles/testing.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testing.dir/main.cpp.obj"
+  "libtesting.dll.a"
+  "testing.exe"
+  "testing.exe.manifest"
+  "testing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
